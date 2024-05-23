542992700bba761e04a55baa5ba75c07ed90c809
Holm der Kannenzwerg <hk@crylle.net>
2024-05-23 10:25:49 +0200

    my aliases added

---
+++ stow/oh-my-zsh/.oh-my-zsh/custom/aliases.zsh
@@ -0,0 +1,16 @@
+alias sa='alias | fzf'
+alias se='printenv | fzf'
+alias s=sudo
+alias svi='sudo vim'
+alias apt='sudo apt'
+alias p='ping6'
+alias q='exit'
+alias tf='tail -f'
+alias tt='telnet'
+alias stowth='stow --dotfiles --verbose --stow --target ~/'
+alias unstowth='stow --dotfiles --verbose --delete --target ~/'
+
+# shutdown
+#
+alias reboot='sudo shutdown -r now'
+alias halt='sudo shutdown -h now'
