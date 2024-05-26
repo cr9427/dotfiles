a7b40d5ccedf7d461e3c17f3860661addcdd23a1
Holm der Kannenzwerg <hk@crylle.net>
2024-05-26 10:45:26 +0200

    added

--- stow/oh-my-zsh/.oh-my-zsh/custom/login.zsh
+++ stow/oh-my-zsh/.oh-my-zsh/custom/login.zsh
@@ -1,13 +1,14 @@
 # determine OS
 #
 if [ -x $HOME/bin/config.guess ]; then
-   _OS=`$HOME/bin/config.guess`
+   OS=`$HOME/bin/config.guess`
 fi
 echo ""
-if [ x$_OS = x ]; then
+if [ x$OS = x ]; then
         echo "OS    is not detectable ... update $HOME/bin/config.guess"
 else
-        echo "OS    is $_OS"
+        echo "OS    is $OS"
+        export OS
 fi
 
 # determine HOME
