c5b710f52603fd29edc089ac490677fe6732dddd
Holm der Kannenzwerg <hk@crylle.net>
2024-05-26 13:08:11 +0200

    --wip-- [skip ci]

--- install.sh
+++ install.sh
@@ -8,5 +8,9 @@ export CHSH=no
 export RUNZSH=no
 sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
 
+if [ -f $HOME/.zshrc ]; then
+  mv $HOME/.zshrc $HOME/.zshrc.old
+fi
+
 cd $HOME/work/GitHub/dotfiles/stow
 stow --dotfiles --verbose --stow --target ~/ *
