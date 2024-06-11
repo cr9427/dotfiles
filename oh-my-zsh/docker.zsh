27db09364b00bfba312bd5445e83e0698c989a6e
Holm der Kannenzwerg <hk@crylle.net>
2024-06-11 10:38:05 +0200

    --wip-- [skip ci]

--- oh-my-zsh/docker.zsh
+++ oh-my-zsh/docker.zsh
@@ -2,7 +2,7 @@
 # Usage: dockrun, or dockrun [centos7|fedora27|debian9|debian8|ubuntu1404|etc.]
 # Run on arm64 if getting errors: `export DOCKER_DEFAULT_PLATFORM=linux/amd64`
 dockrun() {
- docker run -it geerlingguy/docker-"${1:-ubuntu2204}"-ansible /bin/bash
+ docker run -it geerlingguy/docker-"${1:-ubuntu2404}"-ansible /bin/bash
 }
 
 # Enter a running Docker container.
